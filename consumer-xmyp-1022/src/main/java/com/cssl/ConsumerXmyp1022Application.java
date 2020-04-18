2c0ef170555481ae686b71b3679867f90b5eee09
liushuaiwei0701 <liushuaiwei0701@163.com>
2020-04-18 11:44:10 +0800

    GitHub

--- consumer-xmyp-1022/src/main/java/com/cssl/ConsumerXmyp1022Application.java
+++ consumer-xmyp-1022/src/main/java/com/cssl/ConsumerXmyp1022Application.java
@@ -2,11 +2,12 @@ package com.cssl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-
 @EnableFeignClients
 @EnableDiscoveryClient
+@ServletComponentScan(basePackages = "com.cssl.file")
 @SpringBootApplication
 public class ConsumerXmyp1022Application {
 
